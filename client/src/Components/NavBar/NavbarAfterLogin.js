764d568871921d19b47c8c5c8132fb8d4c5e1ae0
Avshalom Mogos <avshalomogos@gmail.com>
2020-06-08 17:02:20 +0300

    added  menu collapse after item is selected to navbar

--- client/src/Components/NavBar/NavbarAfterLogin.js
+++ client/src/Components/NavBar/NavbarAfterLogin.js
@@ -1,34 +1,39 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { Navbar, Nav } from 'react-bootstrap';
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 import { IsUserLoggedContext } from '../../context/IsUserLoggedContext';
+
 
 const NavbarAfterLogin = () => {
     const { setisUserLogged } = useContext(IsUserLoggedContext);
-    const user = JSON.parse(localStorage.jas_login)
+    const [expanded, setExpanded] = useState(false);
+    const user = JSON.parse(localStorage.jas_login);
 
     const logout = () => {
         localStorage.clear();
-        setisUserLogged(false)
+        setisUserLogged(false);
     };
 
     return (
-        <div className="Navbar">
-            <Navbar collapseOnSelect bg="dark" variant="dark" expand="lg">
-                <Navbar.Brand as={Link} to="/">JAS</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link as={Link} to="/applies">Applies</Nav.Link>
-                        <Nav.Link as={Link} to="/about">About</Nav.Link>
+        <div className='Navbar'>
+            <Navbar bg='dark' variant='dark' expand='lg' expanded={expanded}>
+                <Navbar.Brand as={Link} to='/'>JAS</Navbar.Brand>
+                <Navbar.Toggle aria-controls='basic-navbar-nav' onClick={() => setExpanded(expanded ? false : "expanded")}/>
+                <Navbar.Collapse id='basic-navbar-nav'>
+                    <Nav className='mr-auto'>
+                        <Nav.Link onClick={() => setExpanded(false)} as={Link} to='/applies'>Applies</Nav.Link>
+                        <Nav.Link onClick={() => setExpanded(false)} as={Link} to='/about'>About</Nav.Link>
                     </Nav>
-                    <Nav className="Navbar_logginStatus">
-                        <div><strong>Welcome {user.name} ! </strong></div>
-                        <Nav.Link as={Link} onClick={logout} to="/">Logout</Nav.Link>
+                    <Nav className='Navbar_logginStatus'>
+                        <div><strong>Welcome {user.name}!</strong></div>
+                        <Nav.Link onClick={() => {
+                            setExpanded(false)
+                            logout();
+                        }} as={Link} to='/'>Logout</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
         </div>
-    )
+    );
 };
 export default NavbarAfterLogin;
--- client/src/Components/NavBar/NavbarBeforeLogin.js
+++ client/src/Components/NavBar/NavbarBeforeLogin.js
@@ -1,22 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 import './Navbar.css';
 
 
 const NavbarBeforeLogin = () => {
+
+    const [expanded, setExpanded] = useState(false);
+
     return (
         <div className=" Navbar NavbarBeforeLogin">
-            <Navbar collapseOnSelect bg="dark" variant="dark" expand="lg">
-                <Navbar.Brand as={Link} to="/">JAS</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar bg="dark" variant="dark" expand="lg" expanded={expanded}>
+                <Navbar.Brand onClick={() => setExpanded(false)} as={Link} to="/">JAS</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setExpanded(expanded ? false : "expanded")}/>
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
-                        <Nav.Link as={Link} to="/about">About</Nav.Link>
+                        <Nav.Link onClick={() => setExpanded(false)} as={Link} to="/about">About</Nav.Link>
                     </Nav>
                     <Nav className="Navbar_logginStatus">
-                        <Nav.Link as={Link} to="/login">Login</Nav.Link>
-                        <Nav.Link as={Link} to="/signup">Signup</Nav.Link>
+                        <Nav.Link onClick={() => setExpanded(false)} as={Link} to="/login">Login</Nav.Link>
+                        <Nav.Link onClick={() => setExpanded(false)} as={Link} to="/signup">Signup</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
